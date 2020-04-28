26f30cb0c758eb2f28d62dbeafbf05808ac156b5
Jianjun Jin <cndslr@foxmail.com>
2020-04-28 15:39:23 -0400

    update

--- bin/3-1.assembling_50_plastomes_using_NOVOPlasty.sh
+++ bin/3-1.assembling_50_plastomes_using_NOVOPlasty.sh
@@ -147,6 +147,12 @@ perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602587.novoplasty.config.txt
 cd ${RUN_PATH}/NOVOPlasty/K23
 cd SRR5602586-plastome
 perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602586.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K23
+cd SRR2037123-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2037123.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K23
+cd SRR7630500-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR7630500.novoplasty.config.txt
 cd ${RUN_PATH}/NOVOPlasty/
 
 # kmer=31
@@ -294,6 +300,12 @@ perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602587.novoplasty.config.txt
 cd ${RUN_PATH}/NOVOPlasty/K31
 cd SRR5602586-plastome
 perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602586.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K31
+cd SRR2037123-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2037123.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K31
+cd SRR7630500-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR7630500.novoplasty.config.txt
 cd ${RUN_PATH}/NOVOPlasty/
 
 # kmer=39
@@ -441,6 +453,12 @@ perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602587.novoplasty.config.txt
 cd ${RUN_PATH}/NOVOPlasty/K39
 cd SRR5602586-plastome
 perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602586.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K39
+cd SRR2037123-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2037123.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K39
+cd SRR7630500-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR7630500.novoplasty.config.txt
 cd ${RUN_PATH}/NOVOPlasty/
 
 
@@ -590,6 +608,320 @@ cd ${RUN_PATH}/NOVOPlasty/K47
 cd SRR5602586-plastome
 perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602586.novoplasty.config.txt
 cd ${RUN_PATH}/NOVOPlasty/K47
+cd SRR2037123-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2037123.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K47
+cd SRR7630500-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR7630500.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/
+
+# K55/K63 added below
+# kmer=55
+cd K55
+cd ERR1917165-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR1917165.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602594-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602594.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602595-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602595.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602608-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602608.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602609-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602609.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602583-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602583.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602582-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602582.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602602-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602602.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602603-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602603.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd ERR964904-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR964904.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602589-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602589.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602588-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602588.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR6478596-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR6478596.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602584-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602584.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602585-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602585.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602605-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602605.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602604-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602604.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR4428742-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR4428742.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602576-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602576.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602577-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602577.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602593-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602593.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602592-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602592.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR2057084-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2057084.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602599-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602599.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602598-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602598.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd ERR268390-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR268390.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR6932851-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR6932851.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602601-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602601.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602600-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602600.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602581-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602581.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602578-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602578.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602579-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602579.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5028199-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5028199.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602572-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602572.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602573-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602573.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602597-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602597.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602596-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602596.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602610-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602610.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR1145775-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR1145775.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd ERR2206741-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR2206741.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602575-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602575.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602574-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602574.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602590-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602590.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602591-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602591.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602606-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602606.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602607-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602607.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602587-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602587.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR5602586-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602586.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR2037123-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2037123.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K55
+cd SRR7630500-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR7630500.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/
+
+# kmer=63
+cd K63
+cd ERR1917165-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR1917165.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602594-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602594.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602595-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602595.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602608-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602608.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602609-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602609.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602583-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602583.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602582-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602582.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602602-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602602.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602603-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602603.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd ERR964904-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR964904.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602589-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602589.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602588-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602588.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR6478596-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR6478596.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602584-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602584.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602585-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602585.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602605-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602605.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602604-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602604.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR4428742-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR4428742.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602576-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602576.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602577-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602577.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602593-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602593.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602592-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602592.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR2057084-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2057084.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602599-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602599.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602598-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602598.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd ERR268390-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR268390.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR6932851-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR6932851.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602601-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602601.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602600-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602600.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602581-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602581.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602578-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602578.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602579-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602579.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5028199-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5028199.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602572-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602572.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602573-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602573.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602597-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602597.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602596-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602596.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602610-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602610.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR1145775-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR1145775.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd ERR2206741-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c ERR2206741.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602575-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602575.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602574-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602574.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602590-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602590.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602591-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602591.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602606-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602606.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602607-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602607.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602587-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602587.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR5602586-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR5602586.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR2037123-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR2037123.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/K63
+cd SRR7630500-plastome
+perl ../../NOVOPlasty/NOVOPlasty2.7.2.pl -c SRR7630500.novoplasty.config.txt
+cd ${RUN_PATH}/NOVOPlasty/
+
 
 #
 cd ${RUN_PATH}/NOVOPlasty/../
