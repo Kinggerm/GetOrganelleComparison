4d95e8f66c4dc80117bec5847f2af1707fe391c3
Kinggerm <cndslr@foxmail.com>
2020-05-28 01:23:26 -0400

    review_v1

---
+++ bin/5-2.evaluating_GetOrganelle_animal_mitogenomes.sh
@@ -0,0 +1,78 @@
+mkdir GetOrganelle-animal-mitogenome-eval
+
+# create label files
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5620467-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5620467
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--ERR1856354-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base ERR1856354
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5056303-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5056303
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR3879971-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR3879971
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR2179962-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR2179962
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1041081-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1041081
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1581065-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1581065
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5769010-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5769010
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR4340274-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR4340274
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1743021-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1743021
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR4782254-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR4782254
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5132384-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5132384
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR611174-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR611174
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR2132981-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR2132981
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5201683-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5201683
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR305235-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR305235
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1950515-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1950515
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR2318706-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR2318706
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR941879-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR941879
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR2730309-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR2730309
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR3953043-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR3953043
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR955308-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR955308
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5875655-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5875655
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5878339-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5878339
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR4095109-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR4095109
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR847379-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR847379
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR329682-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR329682
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1284940-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1284940
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--DRR014695-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base DRR014695
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5527082-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5527082
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--ERR1952738-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base ERR1952738
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR2737540-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR2737540
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR940268-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR940268
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5860573-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5860573
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR3041429-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR3041429
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR4339879-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR4339879
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR836315-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR836315
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR2717571-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR2717571
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5863004-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5863004
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR3098386-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR3098386
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR039541-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR039541
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--DRR095708-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base DRR095708
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1946581-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1946581
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR136494-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR136494
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1298377-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1298377
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1566160-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1566160
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--ERR466754-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base ERR466754
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1200276-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1200276
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1300141-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1300141
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR748049-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR748049
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR5495100-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR5495100
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1657027-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1657027
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1055280-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1055280
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR1822107-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR1822107
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR747928-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR747928
+slim_fastg.py -F animal_mt `ls GetOrganelle-animal-mitogenome/auto--SRR620242-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-animal-mitogenome-eval --out-base SRR620242
+
+# only keep the label files
+rm GetOrganelle-animal-mitogenome-eval/*.fasta
+
+# summary
+python << END
+import os
+samples = [x for x in os.listdir("GetOrganelle-animal-mitogenome-eval") if x.endswith(".csv")]
+with open("GetOrganelle-animal-mitogenome-eval/summary.tab", "w") as output_h:
+    for s in samples:
+        with open(os.path.join("GetOrganelle-animal-mitogenome-eval", s)) as input_h:
+            head = input_h.readline()
+            genes = []
+            for line in input_h:
+                if line.strip():
+                    genes.extend(line.split("\t")[2].split(","))
+            genes = sorted(set(genes))
+            output_h.write(s.replace(".csv", "") + "\t" + str(len(genes)) + "\t" + ",".join(genes) + "\n")
+END
---
+++ bin/6-2.evaluating_GetOrganelle_fungal_mitogenomes.sh
@@ -0,0 +1,72 @@
+mkdir GetOrganelle-fungal-mitogenome-eval
+
+# generate label table
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803930-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803930
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804139-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804139
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5802419-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5802419
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804120-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804120
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804020-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804020
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803905-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803905
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5764827-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5764827
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804147-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804147
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804015-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804015
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804115-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804115
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5754079-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5754079
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803928-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803928
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5765276-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5765276
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804127-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804127
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5801943-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5801943
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5802181-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5802181
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803933-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803933
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5801947-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5801947
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804112-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804112
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804145-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804145
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5802698-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5802698
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804017-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804017
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804159-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804159
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804023-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804023
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803923-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803923
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803890-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803890
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5764829-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5764829
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803926-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803926
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5764835-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5764835
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5802413-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5802413
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5801934-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5801934
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803916-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803916
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803922-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803922
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804137-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804137
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5801929-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5801929
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803913-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803913
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5801935-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5801935
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5803909-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5803909
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5801933-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5801933
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804119-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804119
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804157-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804157
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5802411-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5802411
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804131-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804131
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5765278-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5765278
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804153-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804153
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5801932-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5801932
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804018-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804018
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5802125-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5802125
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804118-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804118
+slim_fastg.py -F fungus_mt `ls GetOrganelle-fungal-mitogenome/auto--SRR5804130-mitogenome/*.fasta | head -n1` --out-dir GetOrganelle-fungal-mitogenome-eval --out-base SRR5804130
+
+# only keep the label files
+rm GetOrganelle-fungal-mitogenome-eval/*.fasta
+
+# summary
+python << END
+import os
+samples = [x for x in os.listdir("GetOrganelle-fungal-mitogenome-eval") if x.endswith(".csv")]
+with open("GetOrganelle-fungal-mitogenome-eval/summary.tab", "w") as output_h:
+    for s in samples:
+        with open(os.path.join("GetOrganelle-fungal-mitogenome-eval", s)) as input_h:
+            head = input_h.readline()
+            genes = []
+            for line in input_h:
+                if line.strip():
+                    genes.extend(line.split("\t")[2].split(","))
+            genes = sorted(set(genes))
+            output_h.write(s.replace(".csv", "") + "\t" + str(len(genes)) + "\t" + ",".join(genes) + "\n")
+END
